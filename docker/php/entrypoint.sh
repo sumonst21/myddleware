cb75edc60b2594334692c9d16d08cd1ef1a841ca
EstelleMyddleware <estellegaits@myddleware.com>
2022-01-19 15:30:15 +0100

    split apache & php containers but getting a proxy DNS lookup failure : php error which stops exec

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-: ${WWW_DATA_UID:=`stat -c %u /var/www`}
-: ${WWW_DATA_GUID:=`stat -c %g /var/www`}
+: ${WWW_DATA_UID:=$(stat -c %u /var/www)}
+: ${WWW_DATA_GUID:=$(stat -c %g /var/www)}
 
 # Change www-data's uid & guid to be the same as directory in host
 # Fix cache problems
 
 if [ "$WWW_DATA_UID" != "0" ]; then
-    if [ "`id -u www-data`" != "$WWW_DATA_UID" ]; then
+    if [ "$(id -u www-data)" != "$WWW_DATA_UID" ]; then
         usermod -u $WWW_DATA_UID www-data || true
     fi
 
-    if [ "`id -g www-data`" != "$WWW_DATA_GUID" ]; then
+    if [ "$(id -g www-data)" != "$WWW_DATA_GUID" ]; then
         groupmod -g $WWW_DATA_GUID www-data || true
     fi
 fi
 
 # Execute all commands with user www-data
 if [ "$1" = "composer" ]; then
-    su www-data -s /bin/bash -c "`which php` -d memory_limit=-1 `which composer` ${*:2}"
+    su www-data -s /bin/bash -c "$(which php) -d memory_limit=-1 $(which composer) ${*:2}"
 elif [ -n "$1" ]; then
     su www-data -s /bin/bash -c "$*"
 else
@@ -30,3 +30,8 @@ else
         php
     fi
 fi
+
+composer install
+composer update symfony/flex
+php bin/console doctrine:schema:update --force
+php
